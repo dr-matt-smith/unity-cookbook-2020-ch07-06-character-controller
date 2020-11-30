97dde3417ee4267d8d124bbb2eca5f6330797cb5
dr-matt-smith <matt.smith@itb.ie>
2020-11-30 13:38:39 +0000

    added files to project

---
+++ Assets/_Scripts/PlayerMove.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private float vertical = 0;
+    private bool runKeyDown = false;
+    private Animator animator;
+    
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {       
+        // get forward / backward movement from Up/Down/W/S
+        vertical = Input.GetAxis("Vertical");
+
+        // test for RUN / JUMP keys
+        runKeyDown = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+
+    }
+
+    void FixedUpdate()
+    {
+        animator.SetFloat("Speed", vertical);
+        animator.SetBool("Run", runKeyDown);        
+    }
+}
---
+++ Assets/_Scripts/PlayerMoveTurn.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PlayerMoveTurn : MonoBehaviour
+{
+    private float vertical = 0;
+    private float horizontal = 0;
+    private bool runKeyDown = false;
+    private Animator animator;
+    public float rotateSpeed = 1;
+    
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {       
+        // get forward / backward movement from Up/Down/W/S
+        vertical = Input.GetAxis("Vertical");
+
+        // get turn movement from Left/Right/A/D
+        horizontal = Input.GetAxis("Horizontal");
+
+        // test for RUN / JUMP keys
+        runKeyDown = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+
+    }
+
+    void FixedUpdate()
+    {
+        animator.SetFloat("Speed", vertical);
+        animator.SetBool("Run", runKeyDown);
+        
+        // Turn
+        transform.Rotate(0, horizontal * rotateSpeed, 0);
+    }
+}
